9fb4325c1f2160f8f6374d55d39ac0086dc66a45
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2021-01-01 10:08:43 +0300

    Modify transaction list component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function TransactionList() {
   return (
-    <div>
-      Transaction List
-    </div>
+    <>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        <li className="minus">
+          Cash <span>-$400</span> <button className="delete-btn">x</button>
+        </li>
+      </ul>
+    </>
   );
 }
 
