cf85cf9feecffc4d1b8c975fb68640728a1b8fbc
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2021-01-01 10:21:32 +0300

    Modify add transaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 function AddTransaction() {
   return (
-    <div>
-      Add Transaction
-    </div>
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">Amount<br /> (negative - expense, positive - income)</label>
+          <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
   );
 }
 
