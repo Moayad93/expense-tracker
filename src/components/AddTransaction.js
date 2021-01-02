0f975b20076f7e9478ab8b9d8d94bcffecbd3baa
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2021-01-02 08:11:48 +0300

    Add handle change fn & name input to description

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddTransaction() {
+  const [transaction, setTransaction] = useState({
+    description: '',
+    amount: 0
+  });
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setTransaction({ ...transaction, [name]: value });
+  };
+
+  const { description, amount } = transaction;
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="Enter text..." />
+          <label htmlFor="description">Name</label>
+          <input type="text" name="description" id="description" placeholder="Enter text..." onChange={handleChange} value={description} />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount<br /> (negative - expense, positive - income)</label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" name="amount" id="amount" placeholder="Enter amount..." onChange={handleChange} value={amount} />
         </div>
         <button className="btn">Add transaction</button>
       </form>
