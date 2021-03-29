dfba40502a5c888fdc79a3819ec8e80565ec9099
moayad93 <moayad.alnu@gmail.com>
2021-03-29 17:59:52 +0300

    Create delete transaction function globally

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,8 +5,8 @@ function Balance() {
   const { transactions } = useContext(GlobalContext);
   const amounts = transactions.map(transaction => transaction.amount);
   const total = amounts
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+  .reduce((acc, item) => (acc += item), 0)
+  .toFixed(2);
 
   return (
     <>
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,11 @@
 export default function AppReducer(state, action) {
   switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        // dispatched action with a type and payload
+        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      };
     default:
       return state;
   }
